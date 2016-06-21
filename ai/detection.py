15b500da3238a4f2d3aab0fcadff9e7ec5d2b831
BenderV <benderville@gmail.com>
2016-06-21 09:43:03 +0200

    update detection to use local path, simplify function

--- ai/detection.py
+++ ai/detection.py
@@ -1,28 +1,28 @@
-import numpy as np # only for np.pi
 import cv2
 import glob
 import sys
+import os
 
-stop_sign_cascade = cv2.CascadeClassifier('./ressources/haar/stop_sign.xml')
-traffic_lights_cascade = cv2.CascadeClassifier('./ressources/haar/traffic_light.xml')
+local_path = os.path.dirname(os.path.realpath(__file__))
+stop_sign_cascade = cv2.CascadeClassifier(os.path.join(local_path, 'resources/haar/stop_sign.xml'))
+traffic_lights_cascade = cv2.CascadeClassifier(os.path.join(local_path, 'resources/haar/traffic_light.xml'))
 
-def stop_sign_detection(img_gray):
+def stop_sign_detection(img_gray, scaleFactor=1.10, minNeighbors=3, minSize=(50, 50)):
     stop_signs = stop_sign_cascade.detectMultiScale(
         img_gray, 
-        scaleFactor=1.05, 
-        minNeighbors=3, 
-        # minSize=(30, 30), 
+        scaleFactor=scaleFactor, 
+        minNeighbors=minNeighbors,
+        minSize=minSize, 
         # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
         )
-
     return stop_signs
 
-def traffic_light_detection(img_gray):
+def traffic_light_detection(img_gray): 
     traffic_lights = traffic_lights_cascade.detectMultiScale(
         img_gray, 
         scaleFactor=1.05, 
-        minNeighbors=1, 
-        # minSize=(30, 30),
+        minNeighbors=2, 
+        minSize=(30, 30),
         flags=cv2.cv.CV_HAAR_SCALE_IMAGE
         )
     return traffic_lights
@@ -31,44 +31,57 @@ def lines_detection(img_gray):
     img_edges = cv2.Canny(img_gray, 50, 150, apertureSize = 3)
     minLineLength = 100
     maxLineGap = 10
-    lines = cv2.HoughLinesP(img_edges, 1, np.pi/180, 100, minLineLength, maxLineGap)
+    lines = cv2.HoughLinesP(img_edges, 1, 3.1415/180, 100, minLineLength, maxLineGap)
     return lines
 
-def display(img, detect=['stop', 'light', 'line']):
+def detections(img, detect=['stop', 'light', 'lines']): 
+    # Preprocessing
+    # img_ratio = img.shape[1] / float(img.shape[0])
+    # img = cv2.resize(img, (int(480*img_ratio), 480)) # resize every photo
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    detected = {}
+
+    if 'lines' in detect:
+        detected['lines'] = lines_detection(img_gray)
+
+    if 'stop' in detect:
+        detected['stop'] = stop_sign_detection(img_gray)
+
+    if 'light' in detect:
+        detected['light'] = traffic_light_detection(img_gray)
+    
+    return detected
+
+def draw_pattern(img, pattern, style='line', color=(0, 0, 0)):
+    if style == 'line' and len(pattern) != 0:
+        for (x1,y1,x2,y2) in pattern[0]:        
+            cv2.line(img, (x1,y1), (x2,y2), color, 2)
+    else:
+        for (x,y,w,h) in pattern:
+            cv2.rectangle(img, (x,y), (x+w,y+h), color, 2)
+
+    return img 
+
+def display(img, detect=['stop', 'light', 'lines']):
     img_ratio = img.shape[1] / float(img.shape[0])
     img = cv2.resize(img, (int(480*img_ratio), 480)) # resize every photo
-    img_gray = img # cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    stop_signs = stop_sign_detection(img_gray)  if 'stop' in detect else []
-
-    for (x,y,w,h) in stop_signs:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
-        roi_gray = img_gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-
-    traffic_lights = traffic_light_detection(img_gray)
-
-    for (x,y,w,h) in traffic_lights:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-        roi_gray = img_gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-
-    lines = lines_detection(img_gray)
-    if lines is not None:
-        for x1,y1,x2,y2 in lines[0]:
-            cv2.line(img, (x1,y1), (x2,y2), (255, 0, 0), 2)
+    detected = detections(img, detect)
+    print(detected)
+    img = draw_pattern(img, detected.get('stop', []), style='roi', color=(255, 0, 0))
+    img = draw_pattern(img, detected.get('light', []), style='roi', color=(0, 0, 255))
+    img = draw_pattern(img, detected.get('lines', []))
 
     cv2.imshow('img', img)
-    
+
 
 if __name__ == '__main__':
     paths_glob = './resources/traffic_signs/*'
     if len(sys.argv) > 1:
         paths_glob = sys.argv[1]
 
-    print(paths_glob)
     paths = glob.glob(paths_glob)
-    print(paths)
     for path in paths:
         if 'mp4' in path: 
             cam = cv2.VideoCapture(path)            
@@ -83,8 +96,8 @@ if __name__ == '__main__':
                     break
         else:
             img = cv2.imread(path)
-            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            img = cv2.Canny(img_gray, 0, 200) # apertureSize = 3)
+            #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            #img = cv2.Canny(img_gray, 0, 200) # apertureSize = 3)
             display(img)
             cv2.waitKey(0)
     cv2.destroyAllWindows()
--- ai/preprocessing.py
+++ ai/preprocessing.py
@@ -3,16 +3,20 @@ import numpy as np
 import glob
 import sys
 
-def track_line(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def get_mask_color(img, color='red'):
+    # img = cv2.medianBlur(img, 5)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # roi = img[int(img.shape[0]/2):img.shape[0], int(img.shape[1]/2):img.shape[1]] # roi
 
-    #HSV Threshold
-    lowT =(0, 150, 60)
-    highT = (179, 255, 255)
-
-    #HSV filtering
-    mask = cv2.inRange(img, lowT, highT)
-
+    if color == 'red':
+        mask1 = cv2.inRange(hsv, np.array((0,100,100)), np.array((20,255,255))) # yellow
+        mask2 = cv2.inRange(hsv, np.array((80,100,100)), np.array((255,255,255))) # yellow
+        mask = cv2.add(mask1, mask2)
+    elif color == 'blue':
+        mask = cv2.inRange(hsv, np.array((150,100,100)), np.array((190,255,255))) # blue
+    else:
+        mask = cv2.inRange(hsv, np.array((20,100,100)), np.array((60,255,255))) # yellow
+    
     kernel = np.ones((5, 5), np.uint8)
     
     #filtering for image
@@ -21,28 +25,28 @@ def track_line(img):
     mask = cv2.dilate(mask, kernel, iterations=1)
     mask = cv2.erode(mask, kernel, iterations=1)
 
+    return mask
 
-    height, width = mask.shape
-    contours, hierarchy = cv2.findContours(mask, 1, 2)
-    cnt = contours[0]
-    [vx,vy,x,y] = cv2.fitLine(cnt, cv2.cv.CV_DIST_L2, 0,  0.01, 0.01)
-    rows, cols = mask.shape[:2]
-    lefty = int((-x*vy/vx) + y)
-    righty = int(((cols-x)*vy/vx)+y)
-    cv2.line(mask, (cols-1, righty), (0, lefty), (255, 0, 0), 2, cv2.CV_AA)
-    print((cols-1, righty), (0, lefty))
-    cv2.imshow('mask new', mask)
-    # return gradient
+if __name__ == '__main__':
+    images_path = glob.glob(sys.argv[1])
+    for image_path in images_path:
+        image = cv2.imread(image_path)
+        image = cv2.resize(image, (960, 720))
+        mask = get_mask_color(image, color='yellow')
+        mask_color = cv2.cvtColor(mask, cv2.cv.CV_GRAY2RGB)
 
-    # Bitwise-AND mask and original image
-    # res = cv2.bitwise_and(frame, frame, mask=mask)
 
-images_path = glob.glob(sys.argv[1])
-for image_path in images_path:
-    image = cv2.imread(image_path)
-    image = cv2.resize(image, (960, 720))
+        img = mask[:]
+        roi = img[int(img.shape[1]/2):img.shape[0], 0:img.shape[1]] # roi
+        contours, hierarchy = cv2.findContours(roi, 1, 2)
+        M = cv2.moments(contours[0])
+        cx = int(M['m10']/M['m00'])
+        cy = int(img.shape[1]/2) + int(M['m01']/M['m00'])
+        cv2.circle(mask_color, (cx,cy), 10, (0, 0, 255), 3)
+        
 
-    track_line(image)
+        img = cv2.add(image, mask_color)
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
 
-    cv2.waitKey(0)
-cv2.destroyAllWin
+    cv2.destroyAllWindows()
